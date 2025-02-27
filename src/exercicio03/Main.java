516f9539ff1744bdc8e939d200eb42b7a92bb16e
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-27 11:45:30 -0300

    Segundo Commit

---
+++ src/exercicio03/Main.java
@@ -0,0 +1,35 @@
+package exercicio03;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        DecimalFormat dF = new DecimalFormat("0.00");
+        Scanner sc = new Scanner(System.in);
+        Ponto p1 = new Ponto();
+        Ponto p2 = new Ponto();
+
+        System.out.println("Informe as coordenadas do ponto 1: ");
+        p1.x = sc.nextInt();
+        p1.y = sc.nextInt();
+
+        System.out.println("Informe as coordenadas do ponto 2: ");
+        p2.x = sc.nextInt();
+        p2.y = sc.nextInt();
+
+        System.out.println();
+        System.out.println("Distância de p1 para p2: " + dF.format(p1.calcularDistancia(p2)));
+        System.out.println("Distância de p2 para p1: " + dF.format(p2.calcularDistancia(p1)));
+
+        //impressao distancia até origem
+        System.out.println("Distância de p1 a origem: "+ dF.format(p1.calcularDistanciaOrigem()));
+        System.out.println("Distância de p2 a origem: "+ dF.format(p2.calcularDistanciaOrigem()));
+
+        //impressao ponto mais proximo da origem
+        System.out.println();
+        Ponto aux = p1.calcularMenorDistanciaOrigem(p1, p2);
+        System.out.println("Menor distância até a origem entre p1 e p2: "+ aux.formatarPonto());
+
+    }
+}
---
+++ src/exercicio03/Ponto.java
@@ -0,0 +1,30 @@
+package exercicio03;
+
+public class Ponto {
+    int x;
+    int y;
+
+    public double calcularDistancia(Ponto p) {
+        double distancia = Math.sqrt(Math.pow(x - p.x, 2) + Math.pow(y - p.y, 2));
+        return distancia;
+    }
+
+    public double calcularDistanciaOrigem() {
+        Ponto origem = new Ponto();
+        return calcularDistancia(origem);
+    }
+
+    public Ponto calcularMenorDistanciaOrigem(Ponto p1,Ponto p2) {
+        double dp1 = p1.calcularDistanciaOrigem();
+        double dp2 = p2.calcularDistanciaOrigem();
+        if(dp1 < dp2){
+            return p1;
+        }else{
+            return p2;
+        }
+    }
+
+    public String formatarPonto() {
+        return "(" + x + ", "+ y + ")";
+    }
+}
